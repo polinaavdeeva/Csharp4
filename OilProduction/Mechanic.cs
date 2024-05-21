9e9d637b90ad87e4a6556b3a66ae9dafc6f3b0ce
Polina Avdeeva <polinaavdeeva55@gmail.com>
2024-05-22 00:48:53 +0300

    Add files via upload

---
+++ OilProduction/Mechanic.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace OilProduction.Models
+{
+    public class Mechanic
+    {
+        public OilRigStatus FixPlatformFire()
+        {
+            return OilRigStatus.Normal;
+        }
+    }
+}
+
